8419be3ecc107c7ecefb6f21c7d2e0317ab8abf7
Felipe Polato <66489612+felipepolato@users.noreply.github.com>
2021-04-27 10:43:48 -0300

    Screen Register
    Screen Register com Logica em mock

---
+++ todolist/src/screens/Register.js
@@ -0,0 +1,91 @@
+import React from 'react';
+
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+
+export default class Register extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nome: '',
+      sobrenome: '',
+      usuario: '',
+      senha: '',
+    };
+  }
+
+  render() {
+    const {navigation} = this.props;
+
+    return (
+      <View style={styles.container}>
+        <TextInput
+          value={this.state.nome}
+          onChangeText={text => this.setState({nome: text})}
+          placeholder="Insira Seu Nome"
+          style={styles.inputInformation}
+        />
+
+        <TextInput
+          value={this.state.sobrenome}
+          onChangeText={sobrenome => this.setState({senha: sobrenome})}
+          placeholder="Insira Seu Sobrenome"
+          style={styles.inputInformation}
+        />
+
+        <TextInput
+          value={this.state.usuario}
+          onChangeText={usuario => this.setState({senha: usuario})}
+          placeholder="Insira Seu Usuario"
+          style={styles.inputInformation}
+        />
+
+        <TextInput
+          value={this.state.senha}
+          onChangeText={password => this.setState({senha: password})}
+          placeholder="Insira Sua Senha"
+          style={styles.inputInformation}
+        />
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Home')}
+          style={styles.buttonSend}>
+          <Text>Registrar</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ccc',
+  },
+
+  buttonSend: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'yellow',
+    fontSize: 18,
+    marginTop: 30,
+    width: 250,
+    height: 50,
+  },
+
+  inputInformation: {
+    backgroundColor: 'white',
+    fontSize: 18,
+    marginTop: 10,
+    width: 250,
+  },
+});
--- todolist/src/stacks/MainStacks.js
+++ todolist/src/stacks/MainStacks.js
@@ -5,13 +5,14 @@ import Home from '../screens/Home';
 import Login from '../screens/Login';
 import Tarefas from '../screens/Tarefas';
 import Afazeres from '../screens/Afazeres';
+import Register from '../screens/Register';
 
 const Stack = createStackNavigator();
 
 export default () => {
   return (
     <Stack.Navigator
-      initialRouteName="Login"
+      initialRouteName="Register"
       screenOptions={{
         headerShown: false,
       }}>
@@ -19,6 +20,7 @@ export default () => {
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Tarefas" component={Tarefas} />
       <Stack.Screen name="Afazeres" component={Afazeres} />
+      <Stack.Screen name="Register" component={Register} />
     </Stack.Navigator>
   );
 };

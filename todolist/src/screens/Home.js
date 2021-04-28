275ecad5debd94b110c37ba9893a05e47417448f
Felipe Polato <66489612+felipepolato@users.noreply.github.com>
2021-04-28 16:30:06 -0300

    Screen Home
    Estilização

--- todolist/src/screens/Home.js
+++ todolist/src/screens/Home.js
@@ -18,23 +18,25 @@ export default class Home extends React.Component {
 
     return (
       <View style={styles.container}>
-        <TouchableOpacity
-          onPress={() => navigation.navigate('CreateAfazeres')}
-          style={styles.buttonCriar}>
-          <Text style={styles.text1}>Criar A Fazeres</Text>
-        </TouchableOpacity>
+        <View style={styles.container2}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('CreateAfazeres')}
+            style={styles.buttonCriar}>
+            <Text style={styles.text1}>Criar Tarefa</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          onPress={() => navigation.navigate('Afazeres')}
-          style={styles.buttonAfazeres}>
-          <Text style={styles.text}>Tarefas A Fazer</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Afazeres')}
+            style={styles.buttonAfazeres}>
+            <Text style={styles.text1}>Fazer Tarefa</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          onPress={() => navigation.navigate('Tarefas')}
-          style={styles.buttonTarefas}>
-          <Text style={styles.text}>Tarefas Concluidas</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Tarefas')}
+            style={styles.buttonTarefas}>
+            <Text style={styles.text1}>Tarefas Concluidas</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -45,53 +47,59 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#ccc',
+    backgroundColor: '#084d6e',
   },
 
-  tabsNavigator: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    width: Dimensions.get('window').width,
-    padding: 10,
+  container2: {
+    backgroundColor: '#fff',
+    padding: 80,
+    width: Dimensions.get('window').width / 1.4,
+    paddingLeft: 22,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity: 0.41,
+    shadowRadius: 9.11,
+
+    elevation: 14,
   },
 
   buttonTarefas: {
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'yellow',
+    backgroundColor: '#8cb0bf',
     marginTop: 30,
     width: 250,
-    height: 50,
-  },
-
-  buttonCriar: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'black',
-    marginTop: 30,
-    width: 250,
-    height: 50,
-    
+    height: 70,
+    borderRadius: 10
   },
 
   buttonAfazeres: {
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'red',
+    backgroundColor: '#567c99',
     marginTop: 30,
     width: 250,
-    height: 50,
+    height: 70,
+    borderRadius: 10
   },
 
-  text: {
-    fontSize: Dimensions.get('window').width / 18,
-    fontWeight: 'bold',
+  buttonCriar: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#45637a',
+    marginTop: 30,
+    width: 250,
+    height: 70,
+    borderRadius: 10
   },
 
   text1: {
     fontSize: Dimensions.get('window').width / 18,
     fontWeight: 'bold',
-    color: "white"
+    color: 'white',
   },
 });
